b504e870a1ecd9a3cde4e283fa0ed781620dd8c2
paramvir232 <paramvirgrewal232@gmail.com>
2025-07-25 15:11:04 +0530

    Setting UpFile System

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const mongoose = require('mongoose');
-const express = require('express');
-const app = express();
+const Express = require('express');
+// const { json } = require('body-parser');
+
+// Routers
+const userRouter = require('./routes/usersRoute')
+
+const app = Express();
+
 require('dotenv').config();
 mongoose.connect(process.env.MONGODB_URI)
     .then(()=>console.log('Connected to MongoDB'))
     .catch(err=>console.log('Could Not Connect to MongoDB ..',err));
+
+// Middlewares 
+const errorMW = require('./middleware/errorMW')   
+app.use(Express.json()); 
+app.use('/user',userRouter);
+app.use(errorMW);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT,()=>console.log(`Listening port ${PORT} ...`));
